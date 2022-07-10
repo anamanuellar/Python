2476c30f169bed07152a7cea481107f86428d701
Manuella <ana.manuellar@gmail.com>
2022-07-10 11:08:09 -0300

    eighth question

---
+++ question8.py
@@ -0,0 +1,12 @@
+#Escreva um algoritmo que exibe todos os números inteiros entre 0 e 100 que são múltiplos de 7
+
+number = range(100)
+
+for i in number:
+    if i % 7 == 0:
+           print(i)
+
+
+
+
+
